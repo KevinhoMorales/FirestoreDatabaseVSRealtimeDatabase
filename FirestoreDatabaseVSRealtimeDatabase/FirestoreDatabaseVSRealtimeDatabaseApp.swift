71ba9fc5b1205eee85db295d6d2efc2d3dfe8cad
Kevinho Morales <kfmorales94@gmail.com>
2025-10-15 16:33:08 -0500

    Firestore and Realtime added and minor fixes in UI

---
+++ FirestoreDatabaseVSRealtimeDatabase/FirestoreDatabaseVSRealtimeDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreDatabaseVSRealtimeDatabaseApp.swift
+//  FirestoreDatabaseVSRealtimeDatabase
+//
+//  Created by Kevinho Morales on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreDatabaseVSRealtimeDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
